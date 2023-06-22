423c171cd249c36056cec8229834414ffa149d25
jesus <jesus.ramon2192@gmail.com>
2023-06-22 17:25:36 -0600

    Clase 4 endpoints y query parameters

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 console.log('Hello world');
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=236';
 
-fetch(URL)
+/* fetch(API_URL)
     .then(res => res.json())
     .then(data => {
     const img = document.querySelector('img');
     img.src = data[0].url;})
-
-const input = document.querySelector('input');
-input.onclick = getDog;
+ */
+const input1 = document.getElementById('img1');
+const input2 = document.getElementById('img2');
+const input3 = document.getElementById('img3');
 
 async function getDog(){
-    const response = await fetch(URL);
+    const response = await fetch(API_URL);
     const data = await response.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    console.log(data);
+    input1.src = data[0].url;
+    input2.src = data[1].url;
+    input3.src = data[2].url;
+
 }
+
+getDog();
