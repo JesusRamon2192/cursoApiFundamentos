ebb0b1c9b9e065028ac943fe1ce7adbae1e82cd8
jesus <jesus.ramon2192@gmail.com>
2023-06-25 14:35:49 -0600

    Claase 7 maquetacion del proyecto

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 console.log('Hello world');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=236';
+/* Here's your API key:
+
+live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T
+
+Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g. 'api_key=live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T' */
+
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T';
+
+/* const API_URL = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&limit=100&apikey=a00003a2ae815a80abb93c18d5772340&hash=7ec63277d684a0e5222b1f195f89eeb9'; */
+const container = document.querySelector('#marvel-row')
+let contentHTML = '';
+
+//Clave priv 1530013163d257c659eb7f168cfcdc49909bd6ff3a00003a2ae815a80abb93c18d5772340
 
 /* fetch(API_URL)
     .then(res => res.json())
@@ -8,18 +20,33 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=236';
     const img = document.querySelector('img');
     img.src = data[0].url;})
  */
-const input1 = document.getElementById('img1');
-const input2 = document.getElementById('img2');
-const input3 = document.getElementById('img3');
 
-async function getDog(){
+async function reload() {
     const response = await fetch(API_URL);
     const data = await response.json();
     console.log(data);
-    input1.src = data[0].url;
-    input2.src = data[1].url;
-    input3.src = data[2].url;
+    console.log(data[0].url);
+    
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url
+
+    /* for (const hero of data.data.results){
+        let urlHero = hero.urls[0].url;
+        contentHTML = `
+        <div class="col-md-4">
+            <a href="${urlHero}" target="_blank">
+                <img width= 250 src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+            </a>
+            <h3 class="title">${hero.name}</h3>
+        </div>`;
+        container.innerHTML += contentHTML;
+    } */
 
 }
 
-getDog();
+reload();
