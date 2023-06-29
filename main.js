f4098b627744af88687c82efcf0656ad758ba65b
jesus <jesus.ramon2192@gmail.com>
2023-06-28 19:15:16 -0600

    Clase 16 FormData

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const API_URL = 'https://api.thedogapi.com/v1/';
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T';
 const API_URL_FAVOURITES = 'https://api.thedogapi.com/v1/favourites?limit=2&api_key=live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T';
-const API_URL_FAVOURITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T`;
+const API_URL_FAVOURITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}`;
 const API_KEY = 'live_tBR1gbQC08HCpF8Zv1YGZKdaCvOBOBZiDo2ANdzVqYPbtRgTMFJUNALClJxUdT1T';
+
 
 const spanError = document.getElementById('error');
 //Clave priv 1530013163d257c659eb7f168cfcdc49909bd6ff3a00003a2ae815a80abb93c18d5772340
@@ -12,6 +13,7 @@ const spanError = document.getElementById('error');
     const img = document.querySelector('img');
     img.src = data[0].url;})
  */
+
 
 async function loadRandomDogs() {
     const response = await fetch(`${API_URL}images/search?limit=30`);
@@ -24,15 +26,21 @@ async function loadRandomDogs() {
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
         const btn1 = document.getElementById('btn1');
         const btn2 = document.getElementById('btn2');
+        const btn3 = document.getElementById('btn3');
     
         img1.src = data[0].url;
         img2.src = data[1].url;
+        img3.src = data[2].url;
+
 
         btn1.onclick = () => saveFavouritesDog(data[0].id);
         btn2.onclick = () => saveFavouritesDog(data[1].id);
+        btn3.onclick = () => saveFavouritesDog(data[2].id);
     }
+
 
     /* for (const hero of data.data.results){
         let urlHero = hero.urls[0].url;
@@ -47,27 +55,36 @@ async function loadRandomDogs() {
     } */
 }
 
+
 async function loadFavoritesDogs() {
-    const response = await fetch(`${API_URL}favourites?limit=100&api_key=${API_KEY}`);
-    const data = await response.json();
+    const response = await fetch(`${API_URL}favourites?limit=100`, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': `${API_KEY}`
+        }
+    });
     console.log('Favorites');
-    console.log(data);
+    
+
 
     if (response.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + response.status;
     } else {
+        const data = await response.json();
+        console.log(data);
         const section = document.getElementById('favorites');
         const toRender = [];
         section.innerHTML = "";
-        const h2 = document.createElement('h2');
+        /* const h2 = document.createElement('h2');
         const h2Text = document.createTextNode('Favorites Dogs');
         section.appendChild(h2);
-        h2.appendChild(h2Text);
+        h2.appendChild(h2Text); */
         data.forEach( dog => {
             const article = document.createElement('article');
             const img = document.createElement('img');
             const btn = document.createElement('button');
             const btnText = document.createTextNode('Sacar al dog de favoritos');
+
 
             btn.appendChild(btnText);
             btn.onclick = () => deleteFavorite(dog.id);
@@ -80,11 +97,13 @@ async function loadFavoritesDogs() {
     }  
 }
 
+
 async function saveFavouritesDog(id) {
-    const res = await fetch(`${API_URL}favourites?api_key=${API_KEY}`, {
+    const res = await fetch(`${API_URL}favourites`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-API-KEY': `${API_KEY}`
         },
         body: JSON.stringify({
             image_id: id
@@ -94,6 +113,7 @@ async function saveFavouritesDog(id) {
     console.log('Saved');
     console.log(res);
 
+
     if (res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
     } else {
@@ -102,9 +122,13 @@ async function saveFavouritesDog(id) {
     loadFavoritesDogs();
 }
 
+
 async function deleteFavorite(id) {
     const res = await fetch(API_URL_FAVOURITES_DELETE(id), {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: {
+            'X-API-KEY': `${API_KEY}`
+        }
     })
     //const data = await res.json();
     if (res.status !== 200) {
@@ -115,5 +139,29 @@ async function deleteFavorite(id) {
     loadFavoritesDogs();
 }
 
+async function uploadDogPhoto(){
+    const form = document.getElementById('uploadingForm');
+    const formData = new FormData(form);
+
+    console.log(formData.get('file'));
+
+    const res = await fetch(`${API_URL}images/upload`, {
+        method: 'POST',
+        headers: {
+            //'Content-Type': 'multipart/form-data',
+            'X-API-KEY': `${API_KEY}`
+        },
+        body: formData,
+    });
+    if (res.status !== 200 && res.status !== 201) {
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
+        const data = await res.json();
+        console.log('Dog upload');
+        console.log({ data });
+        console.log(data.url);
+        saveFavouritesDog(data.id);
+    }
+}
 loadRandomDogs();
 loadFavoritesDogs();
